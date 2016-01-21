f4a2589311d3430a8dbc43071b97d9acef76e5ee
Adan Ferguson <adanferguson@gmail.com>
2016-01-20 19:29:34 -0700

    Initial setup

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp')
+, inject = require('gulp-inject')
+, wiredep = require('wiredep')
+, nodemon = require('gulp-nodemon');
+
+gulp.task('injectfiles', function() {
+
+    var targetUrl = './www/index.html';
+    wiredep({src: targetUrl});
+
+    var target = gulp.src(targetUrl);
+    var sources = gulp.src(['./www/js/**/*.js', './www/css/**/*.css'], {read: false});
+
+    target.pipe(inject(sources, {ignorePath: 'www'}))
+      .pipe(gulp.dest('./www'));
+
+});
+
+gulp.task('start', function () {
+  nodemon({ script: 'server.js'
+          , ext: 'html ejs js css'
+          , ignore: ['index.html']
+          , tasks: ['injectfiles'] })
+    .on('restart', function () {
+      console.log('restarted!')
+    })
+});
