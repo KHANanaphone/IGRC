60dee41167fc285a632fa51cb8e715750d3b50f1
Adan Ferguson <adanferguson@gmail.com>
2016-01-20 22:45:34 -0700

    Header + routing

--- www/js/app/app.js
+++ www/js/app/app.js
@@ -1,18 +1,43 @@
-var igrcApp = angular.module('igrcApp', [
-  'ngRoute',
-  'igrcControllers'
+var app = angular.module('igrcApp', [
+  'ngRoute'
 ]);
 
-igrcApp.config(['$routeProvider', function($routeProvider){
+app.config(['$routeProvider', function($routeProvider){
 
     $routeProvider
         .when('/about', {
             templateUrl: 'pages/about.html'
         })
+        .when('/', {
+            templateUrl: 'pages/home.html'
+        })
         .otherwise({
             redirectTo: '/'
         });
+}]);
 
+app.controller('igrcController', ['$scope', '$location', function($scope, $location){
 
+    $scope.redirect = function(page){
+
+        $location.url('/' + page);
+    };
+
+    $scope.pages = [
+        'home',
+        'about'
+    ];
+
+    $scope.isActive = function(page){
+
+        var path = $location.path();
+
+        if(path.substr(1, page.length + 1) === page)
+            return true;
+        else if(path == '/' && page == 'home')
+            return true;
+        else
+            return false;
+    };
 
 }]);
